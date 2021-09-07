0c8cb66c6264276aa68a8fa122eaf3bbf1cea8f3
Ajay Gunjal <84914811+Gunjalajay@users.noreply.github.com>
2021-09-07 16:50:15 +0530

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -1,4 +1,4 @@
-//Function insertAtTail,InsertAtHead,deletenode,deleteAtHead
+//Function insertAtTail,InsertAtHead,deletenode,deleteAtHead,sortedInsert,deletenode,reverse.
 #include <iostream>
 using namespace std;
 class node
@@ -49,6 +49,40 @@ void deleteAtHead(node* &head)
     
     delete temp;
 }
+void sortedInsert(node* head, int data) {
+node* x=new node(data);
+//LL -> 1-> 2->   <-4->5
+node*temp=head;
+    if(temp->data > x->data )
+    {
+        x->next=temp;
+        temp->prev=x;
+        head=x;
+    }
+    if(head==NULL)
+    {
+        head=x;
+    }
+    
+    while(temp->next->data < x->data )
+    {
+        temp=temp->next;
+        if(temp->next==NULL)
+        {
+            break;
+        }
+    }
+    if(temp->next!=NULL)
+    {
+        x->next=temp->next;
+    temp->next->prev=x;
+    }
+    temp->next=x;
+    x->prev=temp;
+    
+  //  cout<<temp->data;
+}
+
 void deletenode(node* &head,int pos)
 {
     if(pos==1)
@@ -82,15 +116,46 @@ void display(node* head)
     }
      cout<<"NULL"<<endl;
 }
+void reverse(node* &head)
+{
+    // 1->2->3->4
+    // 4->3->2->1
+    if(head==NULL)
+    {return ;}
+    if(head->next==NULL)
+    {
+        head->next=head->prev;
+        head->prev=NULL;
+        return;
+    }
+    node*curr=head;
+    node*prev1=curr->prev;
+    
+    curr->prev=curr->next;
+    curr->next=prev1;
+    
+    prev1=curr;
+    curr=curr->prev;
+    
+    head=curr;
+    reverse(head);
+    
+}
+
 int main()
 {
    node* head=NULL;
     insertAtTail(head,1);
     insertAtTail(head,2);
     insertAtTail(head,3);
-    insertAtHead(head,4);
+    insertAtTail(head,5);
+    display(head);
+    sortedInsert(head,6);
     display(head);
     deletenode(head,4);
     display(head);
+    reverse(head);
+    display(head);
+    
    return 0;
 }
